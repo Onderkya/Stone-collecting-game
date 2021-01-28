8c28f177b0d43d0ba8291ad255e399ad21e1c95c
Önder Kaya <61360566+Onderkya@users.noreply.github.com>
2021-01-28 15:16:27 +0300

    Stone collecting game

---
+++ Stone collecting game/OdevTest/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace OdevTest
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_basla = new System.Windows.Forms.Button();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btn_TekrarOyna = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lbl_SecilenTas = new System.Windows.Forms.Label();
+            this.lbl_Basarılıtoplama = new System.Windows.Forms.Label();
+            this.lbl_Basarısıztoplama = new System.Windows.Forms.Label();
+            this.lbl_Skor = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btn_basla
+            // 
+            this.btn_basla.Location = new System.Drawing.Point(410, 12);
+            this.btn_basla.Name = "btn_basla";
+            this.btn_basla.Size = new System.Drawing.Size(364, 40);
+            this.btn_basla.TabIndex = 1;
+            this.btn_basla.Text = "Başlat";
+            this.btn_basla.UseVisualStyleBackColor = true;
+            this.btn_basla.Click += new System.EventHandler(this.btn_basla_Click);
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(12, 12);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(392, 531);
+            this.flowLayoutPanel1.TabIndex = 2;
+            // 
+            // btn_TekrarOyna
+            // 
+            this.btn_TekrarOyna.Enabled = false;
+            this.btn_TekrarOyna.Location = new System.Drawing.Point(410, 67);
+            this.btn_TekrarOyna.Name = "btn_TekrarOyna";
+            this.btn_TekrarOyna.Size = new System.Drawing.Size(364, 40);
+            this.btn_TekrarOyna.TabIndex = 1;
+            this.btn_TekrarOyna.Text = "Tekrar Oyna";
+            this.btn_TekrarOyna.UseVisualStyleBackColor = true;
+            this.btn_TekrarOyna.Click += new System.EventHandler(this.btn_basla_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.Location = new System.Drawing.Point(511, 157);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 20);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Seçilen Taş:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label2.Location = new System.Drawing.Point(421, 213);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(185, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Toplam Başarılı Toplama:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.Location = new System.Drawing.Point(404, 260);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(202, 20);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Toplam Başarısız  Toplama:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label4.Location = new System.Drawing.Point(560, 304);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(46, 20);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Skor:";
+            // 
+            // lbl_SecilenTas
+            // 
+            this.lbl_SecilenTas.AutoSize = true;
+            this.lbl_SecilenTas.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_SecilenTas.Location = new System.Drawing.Point(640, 154);
+            this.lbl_SecilenTas.Name = "lbl_SecilenTas";
+            this.lbl_SecilenTas.Size = new System.Drawing.Size(21, 24);
+            this.lbl_SecilenTas.TabIndex = 4;
+            this.lbl_SecilenTas.Text = "0";
+            // 
+            // lbl_Basarılıtoplama
+            // 
+            this.lbl_Basarılıtoplama.AutoSize = true;
+            this.lbl_Basarılıtoplama.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_Basarılıtoplama.Location = new System.Drawing.Point(640, 210);
+            this.lbl_Basarılıtoplama.Name = "lbl_Basarılıtoplama";
+            this.lbl_Basarılıtoplama.Size = new System.Drawing.Size(21, 24);
+            this.lbl_Basarılıtoplama.TabIndex = 4;
+            this.lbl_Basarılıtoplama.Text = "0";
+            // 
+            // lbl_Basarısıztoplama
+            // 
+            this.lbl_Basarısıztoplama.AutoSize = true;
+            this.lbl_Basarısıztoplama.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_Basarısıztoplama.Location = new System.Drawing.Point(640, 257);
+            this.lbl_Basarısıztoplama.Name = "lbl_Basarısıztoplama";
+            this.lbl_Basarısıztoplama.Size = new System.Drawing.Size(21, 24);
+            this.lbl_Basarısıztoplama.TabIndex = 4;
+            this.lbl_Basarısıztoplama.Text = "0";
+            // 
+            // lbl_Skor
+            // 
+            this.lbl_Skor.AutoSize = true;
+            this.lbl_Skor.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lbl_Skor.Location = new System.Drawing.Point(640, 300);
+            this.lbl_Skor.Name = "lbl_Skor";
+            this.lbl_Skor.Size = new System.Drawing.Size(21, 24);
+            this.lbl_Skor.TabIndex = 4;
+            this.lbl_Skor.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(774, 550);
+            this.Controls.Add(this.lbl_Skor);
+            this.Controls.Add(this.lbl_Basarısıztoplama);
+            this.Controls.Add(this.lbl_Basarılıtoplama);
+            this.Controls.Add(this.lbl_SecilenTas);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.btn_TekrarOyna);
+            this.Controls.Add(this.btn_basla);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+         
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button btn_basla;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+        private System.Windows.Forms.Button btn_TekrarOyna;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lbl_SecilenTas;
+        private System.Windows.Forms.Label lbl_Basarılıtoplama;
+        private System.Windows.Forms.Label lbl_Basarısıztoplama;
+        private System.Windows.Forms.Label lbl_Skor;
+    }
+}
+
---
+++ Stone collecting game/OdevTest/Form1.cs
@@ -0,0 +1,446 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OdevTest
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        List<Button> buttonlar = new List<Button>();
+
+
+        void ButonOlustur()
+        {
+            for (int i = 1; i <= 49; i++)
+            {
+                Button btn = new Button();
+                btn.Name = "btn_" + i.ToString();
+                btn.Height = 50;
+                btn.Width = 50;
+                btn.Font = new Font("arial", 25, FontStyle.Bold);
+
+                if (i % 2 == 0)
+                {
+                    btn.BackColor = Color.White;
+                }
+                else
+                {
+                    btn.BackColor = Color.Black;
+                }
+                // btn.UseVisualStyleBackColor = true;
+                btn.Click += Btn_Click;
+                buttonlar.Insert(i, btn);
+                flowLayoutPanel1.Controls.Add(btn);
+                btn_basla.Enabled = false;
+            }
+        }
+        int BasarılıToplama = 1;
+        int basarısız_toplama = 1;
+        private void btn_basla_Click(object sender, EventArgs e)
+        {
+            
+            btn_TekrarOyna.Enabled = true;
+            Button btn123 = new Button();
+            buttonlar.Insert(0, btn123);
+
+            if (flowLayoutPanel1.Controls.Count > 0)
+            {
+                ButonlarınDegerleri.Clear();
+                flowLayoutPanel1.Controls.Clear();
+                LabelTemizle();
+                ButonOlustur();
+                sayac = 0;
+            }
+            else {
+                LabelTemizle();
+                ButonOlustur();
+            }
+
+
+        }
+        int sayac = 0;
+        int butonsayac = 0;
+        List<int> ButonlarınDegerleri = new List<int>();
+        Button btn_Kırmızı = new Button();
+        Button btn = new Button();
+        Button butonson = new Button();
+        private void Btn_Click(object sender, EventArgs e)
+        {
+            Button newbutton = new Button();
+            newbutton = (Button)sender;
+
+
+            //Oyun ilk Başladığında
+
+            if (sayac == 0)
+            {
+                Random rnd = new Random();
+                ButonlarınDegerleri.Clear();
+                Button btn_Kırmızı = (Button)sender;
+                butonson = btn_Kırmızı;
+                btn_Kırmızı.BackColor = Color.Red;
+                string butonisim = btn_Kırmızı.Name;
+                sayac++;
+                int Butonun_Yeri = Convert.ToInt32(butonisim.Substring(4));
+                int butondeger = Butonun_Yeri;
+                MessageBox.Show("Oyun Başladı");
+                ButonlarınDegerleri.Add(butondeger);
+
+
+                for (int i = 1; i <= 6; i++)
+                {
+
+                    Butonun_Yeri = butondeger;
+                    int RastgeleYeri = rnd.Next(0, 49);
+                   
+
+                    while (ButonlarınDegerleri.Contains(RastgeleYeri))
+                    {
+                        RastgeleYeri = rnd.Next(1, 49);
+                        while (Butonun_Yeri == RastgeleYeri)
+                        {
+                            RastgeleYeri = rnd.Next(1, 49);
+                        }
+                    }
+                   
+                    ButonlarınDegerleri.Add(RastgeleYeri);
+
+
+                    Button yenibutton = new Button();
+                    yenibutton = (Button)buttonlar[RastgeleYeri];
+                    yenibutton.Name = "btn_" + RastgeleYeri.ToString();
+                    int Adım_Sayısı = 0;
+
+                    // Aynı sutun ve 7'nin katı demektir..
+                    bool Yediye_bolunuyormu = Butonun_Yeri % 7 == 0 && RastgeleYeri % 7 == 0;
+                    // Aynı sutun ama 7'nin katı değiller.
+                    bool Aynı_sutunlarmı = Butonun_Yeri % 7 == RastgeleYeri % 7;
+
+
+                    
+
+                    if (Aynı_sutunlarmı)
+                    {
+                        int sonuc = 0;
+                        int deger1 = 0;
+                        int deger2 = 0;
+                        if (Yediye_bolunuyormu)
+                        {
+                            sonuc = (Butonun_Yeri / 7) - (RastgeleYeri / 7);
+                            if (sonuc < 0)
+                            {
+                                sonuc *= -1;
+
+                                Adım_Sayısı = sonuc - 1;
+                            }
+                            else if (sonuc == 0)
+                            {
+                                sonuc++;
+                                Adım_Sayısı = sonuc;
+                            }
+                            else
+                            {
+                                Adım_Sayısı = sonuc;
+                            }
+
+                        }
+                        else
+                        {
+                            deger1 = Butonun_Yeri / 7;
+                            if (deger1 < 0)
+                            {
+                                deger1 *= -1;
+                            }
+                            deger2 = RastgeleYeri / 7;
+                            if (deger2 < 0)
+                            {
+                                deger2 *= -1;
+                            }
+
+                            sonuc = deger1 - deger2;
+                            if (sonuc < 0)
+                            {
+                                sonuc *= -1;
+                                Adım_Sayısı = sonuc;
+                            }
+                            else Adım_Sayısı = sonuc;
+
+                        }
+                    }
+
+                    //Farklı sütünlarmı
+                    else if (!Aynı_sutunlarmı)
+                    {
+                        int sonuc = 0;
+                        int deger1 = 0;
+                        int deger2 = 0;
+
+
+
+
+                        int say1 = 0;
+                        bool kontrolbutonilk = Butonun_Yeri % 7 == 0;
+                        bool kontrolbutonson = RastgeleYeri % 7 == 0;
+                        deger1 = (Butonun_Yeri / 7);
+                        if (!kontrolbutonilk)
+                        {
+                            for (int x = 0; x < 6; x++)
+                            {
+
+                                Butonun_Yeri += 1;
+                                say1++;
+                                if (Butonun_Yeri % 7 == 0)
+                                {
+                                    break;
+                                }
+
+
+
+                            }
+
+                        }
+                        else
+                        {
+                            deger1--;
+                        }
+
+
+                        int say2 = 0;
+                        deger2 = (RastgeleYeri / 7);
+                        if (!kontrolbutonson)
+                        {
+                            for (int x = 0; x < 6; x++)
+                            {
+
+                                RastgeleYeri += 1;
+                                say2++;
+                                if (RastgeleYeri % 7 == 0)
+                                {
+                                    break;
+
+                                }
+
+
+
+                            }
+                        }
+                        else { deger2--; }
+
+
+                        int Artıadim = deger1 - deger2;
+                        if (Artıadim < 0)
+                        {
+                            Artıadim *= -1;
+                            sonuc += Artıadim;
+                        }
+                        else { sonuc += Artıadim; }
+
+
+
+                        int gecicisonuc = (say1 - say2);
+                        if (gecicisonuc < 0)
+                        {
+                            gecicisonuc *= -1;
+                            Adım_Sayısı = sonuc + gecicisonuc;
+                        }
+
+
+
+                        Adım_Sayısı = sonuc + gecicisonuc;
+
+
+                    }
+
+                    yenibutton.Text = Adım_Sayısı.ToString();
+                    yenibutton.ForeColor = Color.Red;
+                    yenibutton.Font = new Font("arial", 25, FontStyle.Bold);
+                }
+
+            }
+
+
+            else if (!String.IsNullOrEmpty(newbutton.Text) && butonsayac == 0)
+            {
+
+                //Seçilen butonum
+                btn = newbutton;
+                lbl_SecilenTas.Text = btn.Text;
+                butonsayac = 1;
+
+                // Demekki tıklanması gereken butona tıklanmıs demektir..
+                // burada bir kontrol daha yapılmalı ve ilk buton ile diğer buton karşılatırılacağı için 
+                // ilk butonu bir dizide veya nesnede tutmalı, sonra eğer diğer tıklanan butonun text'i boş ise demekki kullanıcı
+                //doğru şeye tıklamış demektir ve ilk butonun text değeri 1 azaltırıp diğer butona aktarılır böylede hareket etmiş
+                //olur...
+
+
+
+            }
+            else if (!String.IsNullOrEmpty(newbutton.Text) && butonsayac == 1)
+            {
+                MessageBox.Show("Lütfen Boş Bir Yeri Seçiniz Hareket için Boş Biryer Gerekli");
+
+            }
+
+
+            //int bir_x = btn.Location.X;
+            //int bir_y = btn.Location.Y;
+            //int iki_x =  newbutton.Location.X;
+            //int iki_y = newbutton.Location.Y;
+
+
+
+
+
+
+
+
+
+            else if (String.IsNullOrEmpty(newbutton.Text) && butonsayac == 1)
+            {
+                int tıklanan = Convert.ToInt32(btn.Name.Substring(4));
+                int yenitıklanan = Convert.ToInt32(newbutton.Name.Substring(4));
+
+                if (tıklanan + 1 == yenitıklanan || tıklanan - 1 == yenitıklanan || tıklanan + 7 == yenitıklanan || tıklanan - 7 == yenitıklanan)
+                {
+                    string deger = btn.Text;
+                    btn.Text = "";
+                    butonsayac = 0;
+                    //butonu bulduk demektir yani hareket edebiliriz....
+                    bool kontrol = newbutton.Location == butonson.Location;
+                    if (Convert.ToInt32(deger) > 0)
+                    {
+                        deger = (Convert.ToInt32(deger) - 1).ToString();
+                        newbutton.Text = deger;
+                        newbutton.ForeColor = Color.Red;
+
+
+
+                        if (Convert.ToInt32(newbutton.Text) == 0 && kontrol)
+                        {
+
+                            btn.Text = "";
+                            newbutton.Text = "";
+                            lbl_Basarılıtoplama.Text = BasarılıToplama.ToString();
+
+                            lbl_Skor.Text = (Convert.ToInt32(lbl_Skor.Text) + 1).ToString();
+                            MessageBox.Show("Tebrikler taş başarıyla toplandı");
+                            OyunBittimi();
+                            BasarılıToplama++;
+
+
+                        }
+
+                        else if (Convert.ToInt32(newbutton.Text) == 0 && !kontrol)
+                        {
+                            newbutton.Text = "";
+
+                            lbl_Basarısıztoplama.Text = basarısız_toplama.ToString();
+                            lbl_Skor.Text = (Convert.ToInt32(lbl_Skor.Text) - 1).ToString();
+                            MessageBox.Show("Malesef Taş Toplanamadı.");
+                            OyunBittimi();
+                            basarısız_toplama++;
+                        }
+                        else
+                        {
+                            MessageBox.Show("Lütfen Yeniden Taş Seçin");
+
+                        }
+
+                    }
+                    else if ((Convert.ToInt32(newbutton.Text) == 0 && !kontrol))
+                    {
+                        btn.Text = "";
+                        newbutton.Text = "";
+                        MessageBox.Show("Malesef Olmadı.");
+                        OyunBittimi();
+
+                    }
+                    else
+                    {
+                        newbutton.Text = (Convert.ToInt32(btn.Text) - 1).ToString();
+                        newbutton.ForeColor = Color.Red;
+                        btn.Text = "";
+                        MessageBox.Show("Lütfen Yeniden Taş Seçin");
+
+                    }
+
+                }
+                else
+                {
+
+                    MessageBox.Show("Bir Hamlede Tek Kare İlerleyebilirisiniz.");
+                }
+
+
+
+
+
+            }
+            else
+            {
+
+            }
+
+
+
+                }
+                void OyunBittimi()
+                {
+
+                    int sonuc = Convert.ToInt32(lbl_Basarılıtoplama.Text) + Convert.ToInt32(lbl_Basarısıztoplama.Text);
+                    if (sonuc == 6)
+                    {
+                        DialogResult dg = new DialogResult();
+                        dg = MessageBox.Show("Oyun bitti", "Oyun Sona Erdi !", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+                        if (dg == DialogResult.OK)
+                        {
+                            Application.Restart();
+                        }
+                        else
+                        {
+                            Application.Restart();
+
+                        }
+
+
+
+
+                    }
+                }
+
+                void LabelTemizle()
+                {
+
+                    lbl_Basarılıtoplama.Text = 0.ToString();
+                    lbl_Basarısıztoplama.Text = 0.ToString();
+                    lbl_SecilenTas.Text = 0.ToString();
+                    lbl_Skor.Text = 0.ToString();
+                }
+
+
+            }
+        }
+    
+
+        
+
+
+
+       
+    
+
+
+
+
